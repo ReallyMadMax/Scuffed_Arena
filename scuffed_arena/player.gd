28a52315d1846ad3a9ad957d0472eb95cd47d80f
Chunkychick <bownessboy1@gmail.com>
2025-09-21 20:51:05 -0600

    raw changes
    raw changes

--- scuffed_arena/player.gd
+++ scuffed_arena/player.gd
@@ -1,25 +1,51 @@
 extends Area2D
 
-@export var speed = 1000
-
-
+@export var speed = 350
 
 func _ready():
-    print("Hello, world!")
-    print(5*23)
+	print(speed)
 
 func _process(delta):
-    var velocity = Vector2.ZERO # The player's movement vector.
-    if Input.is_action_pressed("move_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("move_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("move_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("move_up"):
-        velocity.y -= 1
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		print(speed)
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		print(speed)
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		print(speed)
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		print(speed)
+	if Input.is_action_pressed("sprint"):
+		speed = 700
+		print(speed)
+	else:
+		speed = 350
+		print(speed)
 
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
-    position += velocity * delta
+	if velocity.x == 0 && velocity.y == 0:
+		$AnimatedSprite2D.play("idle")
+
+	if velocity.y > 0:
+		$AnimatedSprite2D.play("down")
+		$AnimatedSprite2D.flip_h = true
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("up")
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = true
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = false
+
+	position += velocity * delta
