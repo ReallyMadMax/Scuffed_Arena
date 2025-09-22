5195970afd1360e4af300954996cbdacfd39ac19
MrSluginator <fred@dilger.ca>
2025-09-21 20:06:48 -0600

    Added example animation
    The idle_right animation has been filled in with an example, the remaining animations should work once they are put in place.

--- scuffed_arena/player.gd
+++ scuffed_arena/player.gd
@@ -1,25 +1,42 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 400
+var direction : Vector2
 
-
+@onready var animation_tree : AnimationTree = $AnimationTree
 
 func _ready():
-    print("Hello, world!")
-    print(5*23)
+	animation_tree.active = true
+	print("Hello, world!")
+	print(5*23)
 
 func _process(delta):
-    var velocity = Vector2.ZERO # The player's movement vector.
-    if Input.is_action_pressed("move_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("move_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("move_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("move_up"):
-        velocity.y -= 1
+	var dir = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized();
+	if dir:
+		direction = dir
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
 
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
 
-    position += velocity * delta
+	position += velocity * delta
+	update_animation_parameters()
+
+func update_animation_parameters():
+	if (velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/is_idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/is_idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+	
+	if Input.is_action_just_pressed("attack"):
+		animation_tree["parameters/conditions/attack"] = true
+	else:
+		animation_tree["parameters/conditions/attack"] = false
+	
+	animation_tree["parameters/Idle/blend_position"] = direction
+	animation_tree["parameters/Move/blend_position"] = direction
+	animation_tree["parameters/Attack/blend_position"] = direction
