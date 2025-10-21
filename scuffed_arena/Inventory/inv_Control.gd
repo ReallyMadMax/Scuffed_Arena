aa9ee0d9aa27cb2e81477197a652375e78a2841a
Chunkychick <bownessboy1@gmail.com>
2025-10-21 16:57:08 -0600

    Items + Inventory
    blah blah

---
+++ scuffed_arena/Inventory/inv_Control.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var inv: Inv = preload("res://Inventory/playerInv.tres")
+@onready var slots: Array = $GridContainer.get_children()
+var is_open = true
+
+func _ready():
+	print("ready")
+	inv.update.connect(update_slots)
+	update_slots()
+
+func update_slots():
+	for i in range(min(inv.slots.size(), slots.size())):
+		slots[i].update(inv.slots[i])
+
+func _process(_delta):
+	if Input.is_action_just_pressed("OpenInv"):
+		if is_open:
+			close()
+		else:
+			open()
+func open():
+	self.visible = true
+	is_open = true
+func close():
+	visible = false
+	is_open = false
